dbd37de938feaa493651550d70f9dc577f2d3b86
Christian Kester <christiankester77@gmail.com>
2023-05-12 20:28:08 -0700

    verified db connection

--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const port = 3000;
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("postgres://localhost:5432/dbname");
+const sequelize = new Sequelize("postgres://localhost:5432/fitnessDB", {
+    username: "postgres",
+    password: "password",
+});
 
 app.use(
     session({
         secret: "this should be randomized in prod",
+        resave: false,
         saveUninitialized: true,
         cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },
     })
 );
 
-sequelize.sync({ alter: true }).then(
-    app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
-    })
-);
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
